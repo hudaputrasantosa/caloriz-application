98606916806f809bac20a76de83a7e2e4cae8dc4
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2023-11-20 22:16:43 +0700

    [add] hooks predict search calorie

---
+++ src/pages/Predict/hooks/usePredict.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { usePredictContext } from "..";
+import { Config } from "../../../config";
+
+const usePredict = () => {
+  const { setSearch, search } = usePredictContext();
+
+  const searchPredict = async (activity) => {
+    try {
+      const response = await axios.get(
+        `https://api.api-ninjas.com/v1/caloriesburned`,
+        {
+          params: {
+            activity,
+          },
+          headers: {
+            "x-api-key": Config.apiKey,
+          },
+        }
+      );
+      setSearch(response?.data);
+    } catch (error) {
+      console.log("error ->", error);
+    }
+  };
+
+  return {
+    search,
+    searchPredict,
+  };
+};
+
+export default usePredict;
