533632186a66197af249018bdea5a12f5c97d374
hudaputrasantosa <20104087@ittelkom-pwt.ac.id>
2024-01-03 21:49:36 +0700

    [update] card responsive and margin searching

--- src/pages/Predict/ResultPredict.jsx
+++ src/pages/Predict/ResultPredict.jsx
@@ -1,47 +1,50 @@
-import React, { useState } from 'react'
-import SearchPredict from './SearchPredict'
-import usePredict from './hooks/usePredict'
-import { Footer, StickyNavbar } from '../../components'
-import {
-  Card,
-  CardBody,
-  Typography,
-} from "@material-tailwind/react";
+import React, { useState } from "react";
+import SearchPredict from "./SearchPredict";
+import usePredict from "./hooks/usePredict";
+import { Footer, StickyNavbar } from "../../components";
+import { Card, CardBody, Typography } from "@material-tailwind/react";
 
 const ResultPredict = () => {
   const { search } = usePredict();
   const [loading, setLoading] = useState(false);
 
-  const render = () => search && search.map((predict) =>(
-      <Card className="mb-8 w-80">
-      <CardBody>
-        <Typography variant="h5" color="blue-gray" className="mb-2">
-          {predict.name}
-        </Typography>
-        <Typography>
-          Prediksi Kalori : {predict.calories_per_hour}
-        </Typography>
-         <Typography>
-          Durasi Aktifitas : {predict.duration_minutes} Menit
-        </Typography>
-      </CardBody>
-    </Card>
-  ))
+  const render = () =>
+    search &&
+    search.map((predict) => (
+      <Card className="w-full">
+        <CardBody>
+          <Typography
+            variant="h5"
+            color="blue-gray"
+            className="mb-2 lg:text-lg text-sm">
+            {predict.name}
+          </Typography>
+          <Typography className="lg:text-sm text-xs">
+            Prediksi Kalori : {predict.calories_per_hour}
+          </Typography>
+          <Typography className="lg:text-sm text-xs">
+            Durasi Aktifitas : {predict.duration_minutes} Menit
+          </Typography>
+        </CardBody>
+      </Card>
+    ));
 
   return (
     <>
-     <StickyNavbar />
-          <SearchPredict />
-           {loading? ("Loading.. please wait"): (
-            <div className='container mx-auto py-4'>
-            <div className='grid grid-cols-4'>
+      <StickyNavbar />
+      <SearchPredict />
+      {loading ? (
+        "Loading.. please wait"
+      ) : (
+        <div className="container mx-auto my-2 lg:px-8 px-4">
+          <div className="grid lg:grid-cols-4 grid-cols-2 lg:gap-6 gap-3">
             {render()}
-            </div>
-            </div>
-           )}
-        <Footer />
+          </div>
+        </div>
+      )}
+      <Footer />
     </>
-  )
-}
+  );
+};
 
-export default ResultPredict
+export default ResultPredict;
