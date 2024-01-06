b9f0f912a95ef6695490d6e2155b149fcb538d46
hudaputrasantosa <hudaputrasantosa@gmail.com>
2024-01-06 12:53:14 +0700

    [add] filter asc and desc

--- src/pages/Predict/SearchPredict.jsx
+++ src/pages/Predict/SearchPredict.jsx
@@ -2,20 +2,12 @@ import React, { useEffect, useState } from "react";
 import usePredict from "./hooks/usePredict";
 import axios from "axios";
 import { Config } from "../../config";
-import { Input, Typography } from "@material-tailwind/react";
 import Select from "react-select";
 
 const SearchPredict = () => {
   const { searchPredict } = usePredict();
   const [activity, setActivity] = useState([]);
-  const [isSearchable, setIsSearchable] = useState(true);
-  // const [selectedValue, setSelectedValue] = useState(null);
-
-  const handleSearch = (selectOption) => {
-    const data = selectOption.value;
-    console.log(` data is ${data}`);
-    searchPredict(data);
-  };
+  const [sortActivity, setSortActivity] = useState([]);
 
   useEffect(() => {
     axios
@@ -26,16 +18,30 @@ const SearchPredict = () => {
       })
       .then((response) => {
         const activitiesData = response.data.activities;
-        const activity = activitiesData.map((act) => ({
-          value: act,
-          label: act,
-        }));
-        setActivity(activity);
+        setActivity(activitiesData);
       })
       .catch((err) => {
         console.error("error fetch", err);
       });
   }, []);
+
+  const sortMethods = {
+    default: { method: (a, b) => null },
+    ascending: { method: (a, b) => (a.label < b.label ? -1 : 1) },
+    descending: { method: (a, b) => (b.label > a.label ? 1 : -1) },
+  };
+
+  function handleSort(event) {
+    const data = activity.map((act) => ({
+      value: act,
+      label: act,
+    }));
+    setSortActivity(data.sort(sortMethods[event].method));
+  }
+
+  const handleSearch = async (selectOption) => {
+    await searchPredict(selectOption.value);
+  };
 
   return (
     <section className="my-16 mb-20">
@@ -52,18 +58,25 @@ const SearchPredict = () => {
             Sed ut perspiciatis unde omnis iste natus voluptatem accusantium
             doloremque laudantium, totam rem aperiam, eaque ipsa quae.
           </p>
-          <div className="flex flex-col items-center justify-center gap-x-3 space-y-3 sm:space-y-0 lg:w-full px-8">
+          <div className="flex lg:flex-row flex-col items-center justify-center gap-x-3 space-y-3 sm:space-y-0 lg:w-full px-8">
             <Select
               className="basic-single lg:w-96 w-full"
-              classNamePrefix="select"
-              defaultValue={activity[0]}
-              isSearchable={isSearchable}
               name="activity"
-              options={activity}
+              options={sortActivity}
               placeholder="Select Activity .."
-              // value={selectedValue}
               onChange={handleSearch}
             />
+            <select
+              id="sort"
+              onChange={(e) => handleSort(e.target.value)}
+              placeholder="Sort By .."
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block lg:w-fit w-full p-2.5 ">
+              <option value="default" disabled selected>
+                Sort By
+              </option>
+              <option value="ascending">Ascending</option>
+              <option value="descending">Descending</option>
+            </select>
           </div>
         </div>
       </div>
